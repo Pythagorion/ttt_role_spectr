fdb437a7d985c241d31325f06ac84647db8c3777
Blueberryy <igory.ygr200@gmail.com>
2020-10-03 13:09:00 +0500

    Create spectraitor.lua

---
+++ lua/lang/russian/spectraitor.lua
@@ -0,0 +1,10 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SPECTRAITOR.name] = "Предатель"
+L["info_popup_" .. SPECTRAITOR.name] = [[Вы предатель!
+Вы просто нормальный предатель. Возможно, с небольшим секретом. Но ничего особенного.]]
+L["body_found_" .. SPECTRAITOR.abbr] = "Он был предателем."
+L["search_role_" .. SPECTRAITOR.abbr] = "Этот человек был предателем!"
+L["target_" .. SPECTRAITOR.name] = "Предатель"
+L["ttt2_desc_" .. SPECTRAITOR.name] = [[Предателю нужно победить с другими предателями!]]
